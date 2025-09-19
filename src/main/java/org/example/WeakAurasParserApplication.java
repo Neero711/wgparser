375eada3a8ea40d684891e11986faaae4373a17a
Pavel Potupalo <levapnero@gmail.com>
2025-09-19 12:07:28 +0300

    init mservice

--- src/main/java/org/example/WeakAurasParserApplication.java
+++ src/main/java/org/example/WeakAurasParserApplication.java
@@ -2,7 +2,7 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//test
 @SpringBootApplication
 public class WeakAurasParserApplication {
     public static void main(String[] args) {
